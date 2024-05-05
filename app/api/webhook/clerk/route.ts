5ac02e53caeb5a038ce0032f3396bc8aed964041
unknown009d <vbunitynet@gmai.com>
2024-05-05 22:16:04 +0530

    added null to last_name for people with no brains

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
-      lastName: last_name ? last_name : "",
+      lastName: last_name ? last_name : "nulla",
       photo: image_url,
     }
 
